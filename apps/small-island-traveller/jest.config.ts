6012ffa1ad06deecc3352c6cb3069c932b470890
machelslackatcondenast <machel.slack@condenast.com>
2023-12-22 11:34:18 -0500

    Initial commit

---
+++ apps/small-island-traveller/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'small-island-traveller',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/small-island-traveller',
+};
